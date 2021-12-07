59a43322e8220cc39927e790691ee9605e4a56f4
nhaatjlong <longdn555@gmail.com>
2021-12-07 21:24:07 +0700

    .

--- client/src/components/PostList/Post/index.jsx
+++ client/src/components/PostList/Post/index.jsx
@@ -20,52 +20,55 @@ import './style.scss';
 export default function Post({ post }) {
     console.log(post);
     return (
-        <Link
-            className="card-link"
-            to={{ pathname: `/home/postid=${post?.postID}`, state: { post } }}
-            style={{ fontWeight: 'bold' }}
-        >
-            <Card className='card'>
-                <CardHeader
-                    avatar={<Avatar></Avatar>}
-                    title={post.authorId}
-                    subheader={moment(post.updateAt).format("HH:MM MMM DD,YYYY")}
-                    action={
+        <>
+            <Link
+                className="card-link"
+                to={{ pathname: `/home/postid=${post?.postID}`, state: { post } }}
+                style={{ fontWeight: 'bold' }}
+            >
+                <Card className='card'>
+                    <CardHeader
+                        avatar={<Avatar></Avatar>}
+                        title={post.authorId}
+                        subheader={moment(post.updateAt).format("HH:MM MMM DD,YYYY")}
+                        action={
+                            <IconButton>
+                                <MoreVertIcon>
+                                    oke
+                                </MoreVertIcon>
+                            </IconButton>
+                        }
+                    />
+                    <CardMedia
+                        image={img}
+                        title="Title"
+                        style={{ height: "500px", width: "700px", borderRadius: "5px", }}
+                    />
+
+                    <CardActions className='card-action'>
                         <IconButton>
-                            <MoreVertIcon>
-                                oke
-                            </MoreVertIcon>
+                            <FavoriteIcon />
+                            <Typography component="span" color="textSecondary">
+                                {post.likeCount}
+                            </Typography>
                         </IconButton>
-                    }
-                />
-                <CardMedia
-                    image={img}
-                    title="Title"
-                    style={{ height: "500px", width: "700px", borderRadius: "5px", }}
-                />
-
-                <CardActions className='card-action'>
-                    <IconButton>
-                        <FavoriteIcon />
-                        <Typography component="span" color="textSecondary">
-                            {post.likeCount}
+                    </CardActions >
+                    <CardContent className='card-content'>
+                        <Typography cariant="h5" color="textPrimary">
+                            {" "}
+                            {post.title}
                         </Typography>
-                    </IconButton>
-                </CardActions >
-                <CardContent className='card-content'>
-                    <Typography cariant="h5" color="textPrimary">
-                        {" "}
-                        {post.title}
-                    </Typography>
-                    <Typography cariant="body2" component="p" color="textSecondary">
-                        {" "}
-                        {post.content}{" "}
-                    </Typography>
-                </CardContent>
+                        <Typography cariant="body2" component="p" color="textSecondary">
+                            {" "}
+                            {post.content}{" "}
+                        </Typography>
+                    </CardContent>
 
 
 
-            </Card >
-        </Link>
+                </Card >
+            </Link>
+        </>
+
     );
 }
