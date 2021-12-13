f36f2cf2c24ff008e3bdb3ecf40d8cf9e8a0ac47
nhaatjlong <longdn555@gmail.com>
2021-12-13 22:39:25 +0700

    add loader package

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import "./index.scss";
 import { AuthContextProvider } from "./context/AuthContext";
 import reportWebVitals from "./reportWebVitals";
