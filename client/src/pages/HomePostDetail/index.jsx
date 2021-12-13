3c38730d2a85385cd747fc298fe0e0bf2b2ad292
nhaatjlong <longdn555@gmail.com>
2021-12-13 22:38:52 +0700

    Add comment section

--- client/src/pages/HomePostDetail/index.jsx
+++ client/src/pages/HomePostDetail/index.jsx
@@ -1,72 +1,74 @@
-import React,{useState,useEffect} from "react";
-import { useLocation, Link } from 'react-router-dom';
-import './index.scss';
+import React, { useState, useEffect } from "react";
+import { useLocation, Link } from "react-router-dom";
+import "./index.scss";
 // import img from "../../assets/item.jpg";
-import {
-    CardMedia, Avatar
-} from "@material-ui/core";
-import { getTimeDistanceFromNow } from "../../utils/formater"
-import { Button } from '@mui/material';
+import { CardMedia, Avatar } from "@material-ui/core";
+import { getTimeDistanceFromNow } from "../../utils/formater";
+import { Button } from "@mui/material";
 import axios from "axios";
+import CommentBox from "../../components/CommentBox";
 
 function HomePostDetail(props) {
-
     //truyen props thong qua Link
     const location = useLocation();
     const { post } = location.state;
     const [user, setUser] = useState({});
-    
+
     useEffect(() => {
         const fetchUser = async () => {
-          const res = await axios.get(`/users?userId=${post.authorId}`);
-          setUser(res.data);
+            const res = await axios.get(`/users?userId=${post.authorId}`);
+            setUser(res.data);
         };
         fetchUser();
-      }, [post.userId]);
+    }, [post.userId]);
     return (
-        <div className='HomePostDetail'>
-            <div className='left'>
-
+        <div className="PD">
+            <div className="PD-left">
                 <CardMedia
                     image={post.img}
                     title="Title"
-                    style={{ height: "500px", width: "700px", borderRadius: "5px", }} />
-                {/* <section className='body'>
-                   
-                </section> */}
-                 <hr/>
-                <section className='foot'>
-                <br/>
-                </section>
-            </div>
-          
+                    style={{ height: "500px", width: "720px", borderRadius: "5px" }}
+                />
 
-           
-            <div className='comment'>
-                <section className='head'>
-                    <Link to='/' style={{ textDecoration: 'none' }}>
-                        <Button size='small' variant='outlined' ><i class="fas fa-arrow-left"></i>Back to home</Button>
-                    </Link> <br /> <hr />
-
-                    <div className="d-flex">
-                        <Avatar></Avatar>
-                        <div className='head-author mx-3'>{user.username}</div>
-                        <span>{getTimeDistanceFromNow(post.createdAt)}</span>
-                    <Button className='head-button' size='small' variant='outlined'>Theo dõi</Button><br />
+                <hr />
+                <section className="PD-left-comment">
+                    <textarea type='text' className='PD-left-comment-box' placeholder='write your comment here' />
+                    {/* <CommentBox /> */}
+                    <div className='PD-left-comment-button'>
+                        Bình Luận
                     </div>
-                    
                 </section>
-                <h5 className='body-title'>
-                    {post.title}
-                </h5>
-                <p className='body-content'>
-                    {post.content}
-                </p>
-             
+                <br /><br /><br />
             </div>
-            
-        </div>
 
+            <div className="PD-right">
+                <section className="PD-right_head">
+                    <Link to="/" style={{ textDecoration: "none" }}>
+                        <Button size="small" variant="outlined">
+                            <i style={{ marginRight: "10px" }} class="fas fa-arrow-left"></i>
+                            Back to home
+                        </Button>
+                    </Link>{" "}
+                </section> <hr />
+
+                <section className="PD-right_content">
+                    <div className='PD-right-content-head'>
+                        <Avatar></Avatar>
+                        <div className="PD-right-content-head-author">{user.username}</div>
+                        <span className="PD-right-content-head-button">{getTimeDistanceFromNow(post.createdAt)}</span>
+                        <Button size="small" variant="outlined">
+                            Theo dõi
+                        </Button>
+                    </div><br />
+                    <div className='PD-right-content-body'>
+                        <h5 className="PD-right-content-body-title">{post.title}</h5>
+                        <p className="PD-right-content-body-contentPost">{post.content}</p>
+                    </div>
+
+                </section>
+
+            </div>
+        </div>
     );
 }
 
