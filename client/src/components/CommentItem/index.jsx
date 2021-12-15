6295390a493c3b3e1a91a24727d77681b3f17f69
nhaatjlong <longdn555@gmail.com>
2021-12-15 17:37:06 +0700

    Component: add Comment item component

---
+++ client/src/components/CommentItem/index.jsx
@@ -0,0 +1,24 @@
+import { Avatar } from "@mui/material";
+import './style.scss';
+
+export default function CommentItem(props) {
+    const { item } = props;
+
+
+    return <div className='comment-item'>
+        <div className='comment-item-infor'>
+            <Avatar
+                className="comment-item-infor__avatar"
+                src={
+                    item?.authorInfo?.profilePicture
+                        ? item?.authorInfo?.profilePicture
+                        : "person/noAvatar.png"
+                }
+                alt=""
+            />
+            <span className='comment-item-infor__username'>  {item?.authorInfo?.username ? item?.authorInfo?.username : 'Hacker vip pro'}  </span>
+        </div>
+
+        <div className='comment-item-content' >{item?.content} </div>
+    </div>
+}
