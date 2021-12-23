2d52f20d6d9e50d7632f1296588acde7bcc86c7e
chien <c11phamminh@gmail.com>
2021-12-23 14:19:30 +0700

    update

--- client/src/pages/Shop/index.jsx
+++ client/src/pages/Shop/index.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { Products } from "../../api/Products";
+import { Button } from "@mui/material";
 import { Container, Row } from "reactstrap";
 import CartContext from "../../context/Cart";
 import "./index.scss";
@@ -8,12 +9,18 @@ import ShopItem from "../../components/Item/ShopItem";
 import { seo } from "../../utils/seo";
 
 function Shop() {
+  const history = useHistory();
   const { total } = useContext(CartContext);
   useEffect(() => {
     seo("Mua sắm");
   }, []);
   return (
     <div className='Shop'>
+      <div className='CT-head-button' onClick={() => history.push("/newitem")}>
+          <Button size='medium' variant='contained'>
+            Đăng cứu trợ
+          </Button>
+        </div>
       <section className='Shop__head'>
         <Link style={{ textDecoration: "none", color: "black" }} to='/shop/cart'>
           🛒{total}
